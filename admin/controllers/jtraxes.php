fb92baca90128f2cc1b99e1285c4bbf2f5446879
nodenetworks <nodenetworks@users.noreply.github.com>
2020-01-30 00:07:35 +0000

    Sorted out Copyright
    Added copyright for code copied from JTracking

--- admin/controllers/jtraxes.php
+++ admin/controllers/jtraxes.php
@@ -3,6 +3,7 @@
 # JTrax
 # ------------------------------------------------------------------------
 # author    Michał Ostrykiewicz
+# copyright Copyright (C) 2010 Giovanni Mansillo. All Rights Reserved.
 # copyright Copyright (C) 2020 Michał Ostrykiewicz. All rights reserved.
 # @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 # Technical Support:  https://github.com/nodenetworks/jtrax/
