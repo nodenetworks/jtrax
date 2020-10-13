6aea0baac0f2def686aa967337fc0643e8830b9b
ostrykiewicz <mostrykiewicz@borgwarner.com>
2020-10-13 22:39:53 +0100

    XML and SQL updates to allow permission control

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -6,5 +6,11 @@ CREATE TABLE `#__jtrax` (
   `code` varchar(30) NOT NULL,
   `status` varchar(50) NOT NULL,
   `published` tinyint(4) NOT NULL DEFAULT 1,
-   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+  `checked_out` int UNSIGNED NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `access` int UNSIGNED NOT NULL DEFAULT '0',
+   PRIMARY KEY  (`id`),
+   KEY `idx_access` (`access`),
+   KEY `idx_checkout` (`checked_out`);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
---
+++ admin/sql/updates/mysql/1.0.4.sql
@@ -0,0 +1,7 @@
+# com_jtrax
+ALTER TABLE `#__jtrax` ADD COLUMN `checked_out` int(11) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `#__jtrax` ADD COLUMN `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
+ALTER TABLE `#__jtrax` ADD COLUMN `access` int(10) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `#__jtrax` ADD KEY `idx_access` (`access`);
+ALTER TABLE `#__jtrax` ADD KEY `idx_checkout` (`checked_out`);
+ALTER TABLE `#__jtrax` ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
