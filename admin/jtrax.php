6f55a29cecc55956010e47cede607ad5e633fe48
ostrykiewicz <mostrykiewicz@borgwarner.com>
2020-10-14 15:29:41 +0100

    Access control and SQL update - continued

--- admin/jtrax.php
+++ admin/jtrax.php
@@ -10,12 +10,20 @@
 -------------------------------------------------------------------------*/
 
 defined('_JEXEC') or die('Restricted access');
+JHtml::_('behavior.tabstate');
 
 //$document = JFactory::getDocument();
 //$document->addStyleDeclaration('.icon-48-generic {background-image: url(../media/com_jtrax/images/logojtrax.png);}');
 //$document->addStyleDeclaration('.icon-48-jtrax {background-image: url(../media/com_jtrax/images/logojtrax.png);}');
 
 //jimport('joomla.application.component.controller');
+
+
+// Access check: is this user allowed to access the backend of this component?
+if (!JFactory::getUser()->authorise('core.manage', 'com_jtrax')) 
+{
+	throw new JAccessExceptionNotallowed(JText::_('JERROR_ALERTNOAUTHOR'), 403);
+}
 
 $controller = JControllerLegacy::getInstance('JTrax');
 
