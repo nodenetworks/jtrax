27eebce0772e40ad15fb16b31996bd2acef71539
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2025-03-10 23:04:24 +0000

    Update edit.php

--- admin/views/jtrax/tmpl/edit.php
+++ admin/views/jtrax/tmpl/edit.php
@@ -4,14 +4,14 @@
 # ------------------------------------------------------------------------
 # author    Michał Ostrykiewicz
 # copyright Copyright (C) 2010 Giovanni Mansillo. All Rights Reserved.
-# copyright Copyright (C) 2020 Michał Ostrykiewicz. All rights reserved.
+# copyright Copyright (C) 2025 Michał Ostrykiewicz. All rights reserved.
 # @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 # Technical Support:  https://github.com/nodenetworks/jtrax/
 -------------------------------------------------------------------------*/
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
-JHtml::_('behavior.tooltip');
+JHtml::_('bootstrap.tooltip');
 ?>
 
 <form action="<?php echo JRoute::_('index.php?option=com_jtrax&layout=edit&id=' . (int) $this->item->id); ?>"
