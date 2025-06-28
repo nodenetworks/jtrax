825a0df90631d65c29386bf25b101fb9e5732f37
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2025-06-28 23:13:07 +0100

    Update jtrax.php

--- admin/jtrax.php
+++ admin/jtrax.php
@@ -9,15 +9,13 @@
 # Technical Support:  
 -------------------------------------------------------------------------*/
 
-defined('_JEXEC') or die('Restricted access');
+\defined('_JEXEC') or die;
 use \Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
 
 //$document = JFactory::getDocument();
 //$document->addStyleDeclaration('.icon-48-generic {background-image: url(../media/com_jtrax/images/logojtrax.png);}');
 //$document->addStyleDeclaration('.icon-48-jtrax {background-image: url(../media/com_jtrax/images/logojtrax.png);}');
-
-//jimport('joomla.application.component.controller');
 
 $controller = BaseController::getInstance('JTrax');
 
