11dfdbe11f400f4a27da8f80e3b0fc68ca419284
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2025-09-08 00:02:13 +0100

    Updated model file to match Joomla5

--- admin/models/jtrax.php
+++ admin/models/jtrax.php
@@ -10,47 +10,73 @@
 -------------------------------------------------------------------------*/
 
 defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Model\ItemModel;
 
-use \Joomla\CMS\MVC\Model\AdminModel;
-use \Joomla\CMS\Table\Table;
-use \Joomla\CMS\Factory;
-
-
-class JtraxModelJtrax extends AdminModel 
+class JtraxModelJtrax extends ItemModel
 {
-	public function getTable($type = 'JTrax', $prefix = 'JTraxTable', $config = array()) 
-	{
-		return Table::getInstance($type, $prefix, $config);
-	}
-	public function getForm($data = array(), $loadData = true) 
-	{
-		$form = $this->loadForm(
-			'com_jtrax.jtrax',
-			'jtrax',
-		    array(
-			'control' => 'jform',
-			'load_data' => $loadData
-			)
-		);
-		
-		if (empty($form)) 
-		{
-			return false;
-		}
-		return $form;
-	}
-		
-	protected function loadFormData() 
-	{
-		$data = Factory::getApplication()->getUserState(
-		'com_jtrax.edit.jtrax.data',
-		array()
-		);
-		
-		if (empty($data)) 
-		{
-			$data = $this->getItem();
-		}
-		return $data;
-	}
+    /**
+     * Get the search term from input
+     *
+     * @return string
+     */
+    public function getSearchterm()
+    {
+        $jinput = Factory::getApplication()->input;
+        return $jinput->getString('code', '');
+    }
+
+    /**
+     * Get information (tracking results) based on code
+     *
+     * @param string $code
+     * @return array
+     */
+    public function getInformation($code = '')
+    {
+        if (empty($code)) {
+            return [];
+        }
+
+        $db = $this->getDbo();
+        $query = $db->getQuery(true)
+            ->select('*')
+            ->from($db->quoteName('#__jtrax'))
+            ->where($db->quoteName('code') . ' = ' . $db->quote($code))
+            ->where($db->quoteName('published') . ' = 1');
+        $db->setQuery($query);
+
+        return $db->loadObjectList();
+    }
+
+    /**
+     * Get a single item by primary key
+     *
+     * @param int $pk
+     * @return object|false
+     * @throws Exception
+     */
+    public function getItem($pk = null)
+    {
+        $pk = !empty($pk) ? (int)$pk : (int)$this->getState('jtrax.id');
+
+        if ($pk === 0) {
+            return false;
+        }
+
+        $db = $this->getDbo();
+        $query = $db->getQuery(true)
+            ->select('*')
+            ->from($db->quoteName('#__jtrax'))
+            ->where($db->quoteName('id') . ' = ' . (int)$pk);
+        $db->setQuery($query);
+
+        $item = $db->loadObject();
+
+        if (!$item) {
+            throw new Exception('Item not found', 404);
+        }
+
+        return $item;
+    }
 }
