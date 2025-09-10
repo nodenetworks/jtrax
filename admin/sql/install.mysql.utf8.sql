8094bc6eff9509b6663cfeb42ae6d78c9b52fa0e
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2025-09-11 00:27:11 +0100

    Added new table, updated collation and charset

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -4,7 +4,15 @@ CREATE TABLE `#__jtrax` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` date NOT NULL,
   `code` varchar(30) NOT NULL,
+  `status_id` INT(11) NOT NULL DEFAULT 0,
   `status` varchar(50) NOT NULL,
+  `notes` TEXT NULL,
   `published` tinyint(4) NOT NULL DEFAULT 1,
    PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `#__jtrax_statuses` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
