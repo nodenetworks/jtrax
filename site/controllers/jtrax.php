53e1f573f3d5ec8461cb9425581e942ab562d649
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2025-09-19 23:11:44 +0100

    Added missing author/licence header

--- site/controllers/jtrax.php
+++ site/controllers/jtrax.php
@@ -1,5 +1,14 @@
 <?php
-// No direct access
+/*------------------------------------------------------------------------
+# JTrax
+# ------------------------------------------------------------------------
+# author    Michał Ostrykiewicz
+# copyright Copyright (C) 2025 Michał Ostrykiewicz. All rights reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Technical Support:  https://github.com/nodenetworks/jtrax/
+-------------------------------------------------------------------------*/
+
+// No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
